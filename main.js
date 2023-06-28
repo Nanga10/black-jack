f204c5cd6a1177f8ecc206a60f6e938c79fa320f
Nangamso <na.tshwete@gmail.com>
2023-06-28 16:14:14 +1000

    added styling, functionality to reset game at game lose

--- main.js
+++ main.js
@@ -12,54 +12,55 @@ let playEl = document.getElementById("play-round-element");
 let sumEl = document.getElementById("sum-element");
 let statusEl = document.getElementById("status-element");
 
-
-function startGame(){
-    card1 = Math.floor((Math.random() * 13) + 1);
-    card2 = Math.floor((Math.random() * 13)+ 1);
-    arrCards.push(card1, card2);
-    playEl.textContent = "Cards:" + arrCards;
-          
+function startGame() {
+  sumEl.textContent = "Sum:";
+  card1 = Math.floor(Math.random() * 13 + 1);
+  card2 = Math.floor(Math.random() * 13 + 1);
+  arrCards.push(card1, card2);
+  playEl.textContent = "Cards:" + arrCards;
 }
 
-function sumArray (){
-    for (let i = 0; i < arrCards.length; i++) {
-        sum += arrCards[i];
-        }
-          return sumEl.textContent += sum;
+function sumArray() {
+  for (let i = 0; i < arrCards.length; i++) {
+    sum += arrCards[i];
+  }
+  return (sumEl.textContent += sum);
 }
 
-function NewCard(){
-        newCard = Math.floor((Math.random() * 13) + 1);
-        console.log(newCard)
-        if(newCard == 12 || newCard == 13){
-            newCard = 10;
-        } else if (newCard == 1){
-            newCard = 11;
-        }
-        arrCards.push(newCard);
-        playEl.textContent = "Cards:" + arrCards;
-        sum += newCard;
-        sumEl.textContent = "Sum:" + sum;
+function NewCard() {
+  newCard = Math.floor(Math.random() * 13 + 1);
+  console.log(newCard);
+  if (newCard == 12 || newCard == 13) {
+    newCard = 10;
+  } else if (newCard == 1) {
+    newCard = 11;
+  }
+  arrCards.push(newCard);
+  playEl.textContent = "Cards:" + arrCards;
+  sum += newCard;
+  sumEl.textContent = "Sum:" + sum;
+}
 
-    }
+function gameStatus() {
+  if (sum == 21) {
+    statusEl.textContent = "BlackJack! You Win";
+  } else if (sum < 21) {
+    statusEl.textContent = "Draw A New Card";
+  } else {
+    statusEl.textContent = "You Lose";
+    arrCards = [];
+    playEl.textContent = "Play Again?";
+    sum = 0;
+  }
+}
 
-function gameStatus(){
-    if(sum == 21){
-        statusEl.textContent = "BlackJack! You Win"
-    } else if(sum < 20){
-        statusEl.textContent = "Draw A New Card"
-    }else{
-        statusEl.textContent = "You Lose"
-    }
-}    
-
-btnStart.addEventListener("click", function () { 
-startGame();
-sumArray();
-gameStatus();
+btnStart.addEventListener("click", function () {
+  startGame();
+  sumArray();
+  gameStatus();
 });
 
-btnCard.addEventListener("click", function(){
-NewCard();
-gameStatus();
-})
+btnCard.addEventListener("click", function () {
+  NewCard();
+  gameStatus();
+});
